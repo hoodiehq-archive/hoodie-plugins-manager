3f1756e72434e1eaaf9de7a689618390a08e18d3
Caolan McMahon <caolan@caolanmcmahon.com>
2013-07-16 17:21:44 +0100

    added some basic tests for passing config to workers

---
+++ test/lib/utils.js
@@ -0,0 +1,86 @@
+var MultiCouch = require('multicouch'),
+    child_process = require('child_process'),
+    request = require('request'),
+    mkdirp = require('mkdirp'),
+    rimraf = require('rimraf'),
+    async = require('async'),
+    urlParse = require('url').parse;
+
+
+exports.setupCouch = function (opts, callback) {
+    var cmd = 'pkill -fu ' + process.env.LOGNAME + ' ' + opts.data_dir;
+    var pkill = child_process.exec(cmd, function (err, stdout, stderr) {
+        // ignore errors from pkill
+        async.series([
+            async.apply(rimraf, opts.data_dir),
+            async.apply(mkdirp, opts.data_dir),
+            async.apply(startCouch, opts),
+            async.apply(createAdmin, opts)
+        ],
+        function (err, results) {
+            if (err) {
+                return callback(err);
+            }
+            process.on('exit', function (code) {
+                couch.once('stop', function () {
+                    process.exit(code);
+                });
+                couch.stop();
+            });
+            callback(null, couch);
+        });
+    });
+}
+
+function startCouch(opts, callback) {
+    // MultiCouch config object
+    var couch_cfg = {
+        port: urlParse(opts.url).port,
+        prefix: opts.data_dir,
+        couchdb_path: '/usr/bin/couchdb',
+        default_sys_ini: '/etc/couchdb/default.ini',
+        respawn: false // otherwise causes problems shutting down
+    };
+    // starts a local couchdb server using the Hoodie app's data dir
+    var couchdb = new MultiCouch(couch_cfg);
+    // local couchdb has started
+    couchdb.on('start', function () {
+        // give it time to be ready for requests
+        pollCouch(opts, couchdb, function (err) {
+            if (err) {
+                return callback(err);
+            }
+            couch = couchdb;
+            return callback();
+        });
+    });
+    couchdb.on('error', callback);
+    couchdb.start();
+}
+
+function createAdmin(opts, callback) {
+    request({
+        url: opts.url + '/_config/admins/' + opts.user,
+        method: 'PUT',
+        body: JSON.stringify(opts.pass)
+    }, callback);
+}
+
+function pollCouch(opts, couchdb, callback) {
+    function _poll() {
+        var options = {
+            url: opts.url + '/_all_dbs'
+        };
+        request(options, function (err, res, body) {
+            if (res && res.statusCode === 200) {
+                return callback(null, couchdb);
+            }
+            else {
+                // wait and try again
+                return setTimeout(_poll, 100);
+            }
+        });
+    }
+    // start polling
+    _poll();
+}
---
+++ test/test-plugin-api.js
@@ -0,0 +1,98 @@
+var plugins_manager = require('../lib/index'),
+    couchr = require('couchr'),
+    utils = require('./lib/utils'),
+    async = require('async'),
+    url = require('url'),
+    _ = require('underscore');
+
+//require('long-stack-traces');
+
+
+var COUCH = {
+    user: 'admin',
+    pass: 'password',
+    url: 'http://localhost:8985',
+    data_dir: __dirname + '/data',
+};
+
+var DEFAULT_OPTIONS = {
+    couchdb: COUCH
+};
+
+exports.setUp = function (callback) {
+    var that = this;
+    utils.setupCouch(COUCH, function (err, couch) {
+        if (err) {
+            return callback(err);
+        }
+        that.couch = couch;
+
+        var base = url.parse(COUCH.url);
+        base.auth = COUCH.user + ':' + COUCH.pass;
+        base = url.format(base);
+
+        that.base_url = base;
+
+        var appconfig = {
+            config: {foo: 'bar'}
+        };
+        async.series([
+            async.apply(couchr.put, url.resolve(base, 'plugins')),
+            async.apply(couchr.put, url.resolve(base, 'app')),
+            async.apply(couchr.put, url.resolve(base, 'app/config'), appconfig)
+        ],
+        callback);
+    });
+};
+
+exports.tearDown = function (callback) {
+    this.couch.once('stop', callback);
+    this.couch.stop();
+};
+
+exports['get config values from plugin manager'] = function (test) {
+    var plugin_url = url.resolve(this.base_url, 'plugins/plugin%2Fmyplugin');
+    var doc = {
+        config: {asdf: 123}
+    };
+    couchr.put(plugin_url, doc, function (err) {
+        if (err) {
+            return test.done(err);
+        }
+        plugins_manager.start(DEFAULT_OPTIONS, function (err, manager) {
+            if (err) {
+                return test.done(err);
+            }
+            var hoodie = manager.createAPI({name: 'myplugin'});
+            test.equal(hoodie.config.get('foo'), 'bar');
+            test.equal(hoodie.config.get('asdf'), 123);
+            manager.stop(test.done);
+        });
+    });
+};
+
+exports['automatically update plugin config'] = function (test) {
+    plugins_manager.start(DEFAULT_OPTIONS, function (err, manager) {
+        if (err) {
+            return test.done(err);
+        }
+        var hoodie = manager.createAPI({name: 'myplugin'});
+
+        test.equal(hoodie.config.get('foo'), 'bar');
+
+        var url = hoodie._resolve('plugins/plugin%2Fmyplugin');
+        var doc = {config: {foo: 'wibble'}};
+        setTimeout(function () {
+            couchr.put(url, doc, function (err) {
+                if (err) {
+                    return test.done(err);
+                }
+                // test that couchdb change event causes config to update
+                setTimeout(function () {
+                    test.equal(hoodie.config.get('foo'), 'wibble');
+                    manager.stop(test.done);
+                }, 200);
+            });
+        }, 200);
+    });
+};
